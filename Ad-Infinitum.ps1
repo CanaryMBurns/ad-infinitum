f52e8e0de155100ef53ccbbebfd2c0d38d4a6544
Sergio Vegas Alonso <sergio.vegas@mecalux.com>
2020-12-01 17:21:47 +0100

    Example

--- Ad-Infinitum.ps1
+++ Ad-Infinitum.ps1
@@ -1,7 +1,22 @@
 # Config
 $jsonFilePath = '.\data.json'
+$gitAddAllCommand = 'git add .'
+$gitCommitCommand = 'git commit -m "Example"'
 
 # Main
 $json = Get-Content $jsonFilePath | Out-String | ConvertFrom-Json
-Write-Output $json
+
+# Create json object
+$jsonContent = [PSCustomObject]@{
+    '@name'   = 'value4'
+    '@adress' = 'value5'
+}
+
+# Add json object
+$json.data += $jsonContent
+
+# Write in json file
 $json | ConvertTo-Json | Out-File $jsonFilePath
+
+Invoke-Expression $gitAddAllCommand
+Invoke-Expression $gitCommitCommand
